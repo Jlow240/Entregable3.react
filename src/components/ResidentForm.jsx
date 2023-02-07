8fe6c23fec55a2fdc37aab3629a9d9f76f385a74
Juan Luis Orozco <juanluis.ow@gmail.com>
2023-02-07 01:20:35 -0600

    Correccion de header y paginacion

--- src/components/ResidentForm.jsx
+++ src/components/ResidentForm.jsx
@@ -4,14 +4,16 @@ import "./Styles/ResidentForm.css"
 const ResidentForm = ({handleSubmit}) => {
     return (
         <section className='residentForm'>
-            <h1 className='ResidentForm__title'>
-                <img src="src\assets\Rick_and_Morty.png" alt="TITULO RICK Y MORTY" />
+            <h1 className='residentForm__title'>
+                <img className='residentForm__title-img' src="src\assets\Rick_and_Morty2.png" alt="TITULO RICK Y MORTY" />
             </h1>
         <form className='residentForm__form' onSubmit={handleSubmit}>
             <input className='residentForm__input ' type="text" id='idLocation' placeholder='Look for a universe (1 to 126)'/>
             <label className='residentForm__lbl'>
                 <span className='residentForm__lbl-txt'></span>
             </label>
+                    
+            <button className='residentForm__btn'></button>
         </form>
         </section>
     )
