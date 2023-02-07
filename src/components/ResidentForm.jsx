a7c16e023b34abde53878f1b4fea8877158a5d33
Juan Luis Orozco <juanluis.ow@gmail.com>
2023-02-06 20:12:29 -0600

    Cambio de Header

--- src/components/ResidentForm.jsx
+++ src/components/ResidentForm.jsx
@@ -5,7 +5,7 @@ const ResidentForm = ({handleSubmit}) => {
     return (
         <section className='residentForm'>
             <h1 className='ResidentForm__title'>
-                <img src="src\assets\Rick_and_Morty.svg.png" alt="" />
+                <img src="src\assets\Rick_and_Morty.png" alt="TITULO RICK Y MORTY" />
             </h1>
         <form className='residentForm__form' onSubmit={handleSubmit}>
             <input className='residentForm__input ' type="text" id='idLocation' placeholder='Look for a universe (1 to 126)'/>
